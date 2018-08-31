38c12b83bb97410a98f6741030d66fd5532fe566
Cyprinus <Cyprinus12138@gmail.com>
2018-09-01 00:10:22 +0800

    Fix collection

--- collection/serializers.py
+++ collection/serializers.py
@@ -1,38 +1,8 @@
 from rest_framework import serializers
 from .models import Collection
-from collection.services import CollectionServices
-
-
-class CollectionsListSerializer(serializers.ListSerializer):
-    def create(self, validated_data):
-        collections = [Collection(**item) for item in validated_data]
-        return Collection.objects.bulk_create(collections)
 
 
 class CollectionSerializer(serializers.ModelSerializer):
-    abstract = serializers.SerializerMethodField(read_only=True)
-    thumb = serializers.SerializerMethodField(read_only=True)
-    context = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Collection
-        list_serializer_class = CollectionsListSerializer
-        fields = ['abstract', 'thumb', 'context']
-
-    def get_abstract(self, obj):
-        collections = CollectionServices.get_collection_list_by_user(obj)
-        return collections and [collection.abstract for collection in collections]
-
-    def get_thumb(self, obj):
-        collections = CollectionServices.get_collection_list_by_user(obj)
-        return collections and [collection.thumb for collection in collections]
-
-    def get_context(self, obj):
-        collections = CollectionServices.get_collection_list_by_user(obj)
-        return collections and [collection.context for collection in collections]
-
-
-class CreateCollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['owner', 'abstract', 'thumb', 'context']
--- collection/views.py
+++ collection/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions, response, status
 from rest_framework.decorators import action
 from collection.models import Collection
-from collection.serializers import CollectionSerializer,CreateCollectionSerializer
+from collection.serializers import CollectionSerializer
 from utils.permissions import get_only_owner_can_write
 from collection.services import CollectionServices
 
@@ -10,8 +10,10 @@ from collection.services import CollectionServices
 class CollectionViewSets(ModelViewSet):
     queryset = Collection.objects.all()
     permission_classes = [permissions.IsAuthenticated, get_only_owner_can_write('owner')]
-    serializer_class = CreateCollectionSerializer
+    serializer_class = CollectionSerializer
 
     @action(detail=False, permission_classes=(permissions.IsAuthenticated,))
     def my_collections(self, request):
-        return response.Response(CollectionSerializer(request.user).data, status=status.HTTP_200_OK)
+        queryset = CollectionServices.get_collection_list_by_user(request.user)
+        serializer = CollectionSerializer(queryset,many=True)
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
